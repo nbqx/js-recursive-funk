d3e9c9a3cf6b65c26f95d1e1f99a5ba0054fb95d
nbqx <nbqxnbq@gmail.com>
2012-06-08 14:54:25 +0900

    modified osx.js for less cpu use

--- js/osx.js
+++ js/osx.js
@@ -3,6 +3,10 @@ OSX = this.OSX = {};
 OSX.funk_wav = T("wav", "./audio/funk.wav", false).load();
 OSX.funk = function(){
   T("timeout", _(16), function(){
-    OSX.funk_wav.clone().play();
+    var s = OSX.funk_wav.clone();
+    s.onended = function(){
+      s.off();
+    };
+    s.play();
   }).on();
 };
